417340c84feda2075e72187b765e97765e1fcc55
Minty Seck <mintyseck@gmail.com>
2026-06-22 12:08:16 +0000

    J7 rectification Pieces Jointes

--- src/models/PieceJointe.js
+++ src/models/PieceJointe.js
@@ -1,30 +1,42 @@
 const { DataTypes } = require('sequelize');
+
 module.exports = (sequelize) => {
-  const PieceJointe = sequelize.define('PieceJointe', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+  const PieceJointe = sequelize.define(
+    'PieceJointe',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      ticket_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nom_fichier: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      taille_ko: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
     },
-    ticket_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    nom_fichier: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    taille_ko: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-  });
+    {
+      tableName: 'PiecesJointes',
+      freezeTableName: true,
+    }
+  );
+
   PieceJointe.associate = (models) => {
-    PieceJointe.belongsTo(models.Ticket, { foreignKey: 'ticket_id' });
+    PieceJointe.belongsTo(models.Ticket, {
+      foreignKey: 'ticket_id',
+    });
   };
+
   return PieceJointe;
 };
