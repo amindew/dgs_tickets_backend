cd7e0995cb6e858486322060a01a6535d5980432
Minty Seck <mintyseck@gmail.com>
2026-07-17 16:38:37 +0000

    modele Notifications

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -7,28 +7,61 @@ module.exports = (sequelize) => {
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
+
     user_id: {
       type: DataTypes.UUID,
       allowNull: false,
     },
+
     ticket_id: {
       type: DataTypes.UUID,
       allowNull: true,
     },
+
     type: {
-      type: DataTypes.ENUM('statut_change', 'assignation', 'commentaire','piece_jointe'),
+      type: DataTypes.ENUM(
+        'statut_change',
+        'assignation',
+        'commentaire',
+        'piece_jointe'
+      ),
       allowNull: false,
     },
-    titre:      { type: DataTypes.STRING,  allowNull: false },
-    message:    { type: DataTypes.TEXT,    allowNull: false },
-    lue:        { type: DataTypes.BOOLEAN, defaultValue: false },
-    date_envoi: { type: DataTypes.DATE,    defaultValue: DataTypes.NOW },
+
+    titre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
+    lue: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+
+  }, {
+    tableName: 'Notifications',
+    timestamps: true,
   });
 
+
   Notification.associate = (models) => {
-    Notification.belongsTo(models.User,   { foreignKey: 'user_id',   as: 'destinataire' });
-    Notification.belongsTo(models.Ticket, { foreignKey: 'ticket_id', as: 'ticket' });
+    Notification.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'destinataire'
+    });
+
+    Notification.belongsTo(models.Ticket, {
+      foreignKey: 'ticket_id',
+      as: 'ticket'
+    });
   };
+
 
   return Notification;
 };
